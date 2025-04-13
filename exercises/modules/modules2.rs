b61fa6f61061716af599aa23e2ef2d60b46b5154
Homey Lau <lhw1953927853@gmail.com>
2025-04-13 17:55:10 +0800

    update modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -6,14 +6,10 @@
 //
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
+use self::fruits::PEAR as fruit;
+use self::veggies::CUCUMBER as veggie;
 
 mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
-
     mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
