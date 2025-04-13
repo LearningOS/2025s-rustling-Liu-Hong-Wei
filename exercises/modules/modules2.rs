04ead863a4f952c4e5919a053cfc8d209696bf0d
homeylau <lhw1953927853@gmail.com>
2025-04-13 18:09:22 +0800

    fix modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -6,16 +6,16 @@
 //
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
-use self::fruits::PEAR as fruit;
-use self::veggies::CUCUMBER as veggie;
+use crate::delicious_snacks::fruits::PEAR as fruit;
+use crate::delicious_snacks::veggies::CUCUMBER as veggie;
 
 mod delicious_snacks {
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
@@ -24,7 +24,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        fruit,
+        veggie
     );
 }
